8393001b7f9a935da638a2065a729439b06d752c
Arterning <ning.huang.ridson@gmail.com>
2024-02-02 09:13:18 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use std::process;
 
 use std::error::Error;
 use minigrep::search;
+use minigrep::search_case_insensitive;
 
 
 struct Config {
     query: String,
     file_path: String,
+    pub ignore_case: bool,
+
 }
 
 impl Config {
@@ -20,8 +23,10 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Config { query, file_path }
+
+        Config { query, file_path, ignore_case }
     }
 
     fn build(args: &[String]) -> Result<Config, &'static str> {
@@ -32,15 +37,19 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
 fn parse_config(args: &[String]) -> Config {
     let query = &args[1];
     let file_path = &args[2];
+    let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-    Config { query: query.to_string(), file_path: file_path.to_string() }
+
+    Config { query: query.to_string(), file_path: file_path.to_string(), ignore_case }
 }
 
 // cargo run -- bog poem.txt
@@ -69,10 +78,14 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
 
-    for line in search(&config.query, &contents) {
-        println!("Search line :{line}");
+    for line in results {
+        println!("Search : {line}");
     }
 
     Ok(())
