fffaadeb815b74fb8552917e790b1c6be544110d
Arterning <ning.huang.ridson@gmail.com>
2024-02-02 08:46:30 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,26 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Config {
+
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
         Config { query, file_path }
+    }
+
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
     }
 }
 
